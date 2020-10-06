5c44d271a2ea1ed4078e43f4cdcee3126dad4420
teodorakocan <53410490+teodorakocan@users.noreply.github.com>
2020-10-06 23:41:38 +0200

    refactor after review

--- index.js
+++ index.js
@@ -1,177 +1,180 @@
 const TASK_KEY = 'tasks';
-const index = 0;
-const buttonAll = document.getElementById("all");
-const buttonCompleted = document.getElementById("completed");
-const buttonActive = document.getElementById("active");
-const tasksString = localStorage.getItem("tasks");
+const ACTIVE_CLASS = 'active';
+
 let tasks = [];
 let numberOfActiveTasks = 0;
 
-if(!tasksString){
-    localStorage.setItem(TASK_KEY, tasks);
-}else{
-    tasks = JSON.parse(tasksString);
-    localStorage.setItem(TASK_KEY, JSON.stringify(tasks));
-    for(let i = 0; i< tasks.length; i++){
-        addTask(tasks[i]);
-        if(!tasks[i].completed){
-            numberOfActiveTasks++;
-        }
-    }
-}
+const buttonAll = document.getElementById('all');
+const buttonCompleted = document.getElementById('completed');
+const buttonActive = document.getElementById(ACTIVE_CLASS);
 
-let numb = document.createElement('label');
-numb.setAttribute("id", "numberOfActiveTasks");
-numb.innerText = numberOfActiveTasks + " items left";
-document.querySelector("#number").appendChild(numb);
+const labelNumberOfActiveTasks = document.createElement('label');
+document.querySelector('#number').appendChild(labelNumberOfActiveTasks);
 
-let input = document.getElementById('addTask');
-input.addEventListener("keyup", function(event) {
+const input = document.getElementById('addTask');
+input.addEventListener('keyup', function (event) {
     if (event.keyCode === 13) {
         event.preventDefault();
-        if(input.value !== ''){
-            task = {name: input.value, completed: false}
-            numberOfActiveTasks++;
-            tasks.push(task);
-            addTask(task);
-            localStorage.setItem(TASK_KEY, JSON.stringify(tasks));
-            event.currentTarget.value = "";
-            reloadNumberOfActiveTasks();
+        if (input.value !== '') {
+            addTask(input.value);
+            event.currentTarget.value = '';
         }
     }
 });
 
+init();
 
-function reloadNumberOfActiveTasks(){
-    numb.innerText = numberOfActiveTasks + " items left";
-}
+function init() {
+    const tasksString = localStorage.getItem('tasks');
 
-function addTask(value){
-    let newTask = document.createElement('div');
-    newTask.setAttribute("id", "div_"+ tasks.indexOf(value));
-    newTask.style.backgroundColor="white";
-    newTask.classList.add("task");
-    document.querySelector("#tasks").appendChild(newTask);
-    
-    let input = document.createElement('input');
-    input.setAttribute("type", "checkbox");
-    input.setAttribute("id", "input_"+ tasks.indexOf(value));
-    input.classList.add("checkbox")
-    input.onchange = function(){
-        crossTask(value);
-    };
-    document.querySelector("#div_" + tasks.indexOf(value)).appendChild(input);
-
-    let task = document.createElement('label');
-    task.innerText = value.name;
-    task.setAttribute("id", tasks.indexOf(value));
-    task.classList.add("taskarea")
-    document.querySelector("#div_" + tasks.indexOf(value)).appendChild(task);
-
-    let deletebtn = document.createElement('button');
-    deletebtn.innerHTML = '<i class = "fa fa-close"></i>';
-    deletebtn.setAttribute("id", "deletebtn");
-    deletebtn.onclick = function(){
-        deleteTask(value);
-    };
-    document.querySelector("#div_" + tasks.indexOf(value)).appendChild(deletebtn);
-
-    if(value.completed){
-        task.style.textDecoration = "line-through"
-        input.checked = true;
+    if (!tasksString) {
+        localStorage.setItem(TASK_KEY, []);
+    } else {
+        tasks = JSON.parse(tasksString);
+        localStorage.setItem(TASK_KEY, JSON.stringify(tasks));
+        for (let i = 0; i < tasks.length; i++) {
+            renderTask(tasks[i]);
+            if (!tasks[i].completed) {
+                numberOfActiveTasks++;
+            }
+        }
     }
+    renderNumberOfActiveTasks();
 }
 
-function deleteTask(value){
-    let deleted = [];
-    let index = tasks.indexOf(value);
-    for(let i = 0; i < tasks.length; i++){
-        if(index != i){
+function addTask(name) {
+    const task = { name: name, completed: false };
+    numberOfActiveTasks++;
+    tasks.push(task);
+    renderTask(task);
+    localStorage.setItem(TASK_KEY, JSON.stringify(tasks));
+    renderNumberOfActiveTasks();
+}
+
+function deleteTask(value) {
+    const deleted = [];
+    const index = tasks.indexOf(value);
+
+    if (!tasks[index].completed) {
+        numberOfActiveTasks--;
+    }
+
+    for (let i = 0; i < tasks.length; i++) {
+        if (index != i) {
             deleted.push(tasks[i]);
         }
     }
 
     tasks = deleted;
     localStorage.setItem(TASK_KEY, JSON.stringify(tasks));
-    location.reload();
+    renderTasks();
+    renderNumberOfActiveTasks();
 }
 
-function crossTask(value){
-    let number = document.getElementById("number");
-    if(document.getElementById("input_" + tasks.indexOf(value)).checked){
-        tasks[tasks.indexOf(value)].completed = document.getElementById("input_" + tasks.indexOf(value)).checked;
-        localStorage.setItem(TASK_KEY, JSON.stringify(tasks));
+function crossTask(value) {
+    const checked = document.getElementById('input_' + tasks.indexOf(value)).checked;
+    if (checked) {
         numberOfActiveTasks--;
-    }
-    else{
-        tasks[tasks.indexOf(value)].completed = document.getElementById("input_" + tasks.indexOf(value)).checked;;
-        localStorage.setItem(TASK_KEY, JSON.stringify(tasks));
+    } else {
         numberOfActiveTasks++;
     }
 
-    
-    number.innerText = numberOfActiveTasks + " items left"
-    checkFilter();
-}
+    tasks[tasks.indexOf(value)].completed = checked;
+    localStorage.setItem(TASK_KEY, JSON.stringify(tasks));
 
-function checkFilter(){
-    if(buttonAll.classList.contains("active")){
+    renderNumberOfActiveTasks();
+
+    if (buttonAll.classList.contains(ACTIVE_CLASS)) {
         allTasks();
-    }else if(buttonCompleted.classList.contains("active")){
+    } else if (buttonCompleted.classList.contains(ACTIVE_CLASS)) {
         completedTasks();
-    }else if(buttonActive.classList.contains("active")){
+    } else if (buttonActive.classList.contains(ACTIVE_CLASS)) {
         activeTasks();
     }
 }
 
-function allTasks(){
-    buttonAll.classList.add("active");
-    buttonCompleted.classList.remove("active");
-    buttonActive.classList.remove("active");
+function allTasks() {
+    buttonAll.classList.add(ACTIVE_CLASS);
+    buttonCompleted.classList.remove(ACTIVE_CLASS);
+    buttonActive.classList.remove(ACTIVE_CLASS);
 
-    let allTasks = document.getElementById("tasks");
-    while(allTasks.children.length > 0){
-        let obj = document.getElementById(allTasks.children[index].id);
-        obj.remove();
-    }
-
-    for(let i = 0; i < tasks.length; i++){
-        addTask(tasks[i]);
+    clearTaskElements();
+    for (let i = 0; i < tasks.length; i++) {
+        renderTask(tasks[i]);
     }
 }
 
-function activeTasks(){
-    buttonAll.classList.remove("active");
-    buttonCompleted.classList.remove("active");
-    buttonActive.classList.add("active");
+function activeTasks() {
+    buttonAll.classList.remove(ACTIVE_CLASS);
+    buttonCompleted.classList.remove(ACTIVE_CLASS);
+    buttonActive.classList.add(ACTIVE_CLASS);
 
-    let allTasks = document.getElementById("tasks");
-    while(allTasks.children.length > 0){
-        let obj = document.getElementById(allTasks.children[index].id);
-        obj.remove();
-    }
+    clearTaskElements();
 
-    for(let i = 0; i < tasks.length; i++){
-        if(!tasks[i].completed){
-            addTask(tasks[i]);
+    for (let i = 0; i < tasks.length; i++) {
+        if (!tasks[i].completed) {
+            renderTask(tasks[i]);
         }
     }
 }
 
-function completedTasks(){
-    buttonAll.classList.remove("active");
-    buttonCompleted.classList.add("active");
-    buttonActive.classList.remove("active");
+function completedTasks() {
+    buttonAll.classList.remove(ACTIVE_CLASS);
+    buttonCompleted.classList.add(ACTIVE_CLASS);
+    buttonActive.classList.remove(ACTIVE_CLASS);
 
-    let allTasks = document.getElementById("tasks");
-    while(allTasks.children.length > 0){
-        let obj = document.getElementById(allTasks.children[index].id);
-        obj.remove();
-    }
+    clearTaskElements();
 
-    for(let i = 0; i < tasks.length; i++){
-        if(tasks[i].completed){
-            addTask(tasks[i]);
+    for (let i = 0; i < tasks.length; i++) {
+        if (tasks[i].completed) {
+            renderTask(tasks[i]);
         }
+    }
+}
+
+function clearTaskElements() {
+    const allTaskElements = document.getElementById('tasks');
+    while (allTaskElements.children.length > 0) {
+        document.getElementById(allTaskElements.children[0].id).remove();
+    }
+}
+
+function renderNumberOfActiveTasks() {
+    labelNumberOfActiveTasks.innerText = numberOfActiveTasks + ' items left';
+}
+
+function renderTask(value) {
+    const newTask = document.createElement('div');
+    newTask.setAttribute('id', 'div_' + tasks.indexOf(value));
+    newTask.style.backgroundColor = 'white';
+    newTask.classList.add('task');
+    document.querySelector('#tasks').appendChild(newTask);
+
+    const input = document.createElement('input');
+    input.setAttribute('type', 'checkbox');
+    input.setAttribute('id', 'input_' + tasks.indexOf(value));
+    input.classList.add('checkbox')
+    input.onchange = function () {
+        crossTask(value);
+    };
+    document.querySelector('#div_' + tasks.indexOf(value)).appendChild(input);
+
+    const task = document.createElement('label');
+    task.innerText = value.name;
+    task.setAttribute('id', tasks.indexOf(value));
+    task.classList.add('taskarea')
+    document.querySelector('#div_' + tasks.indexOf(value)).appendChild(task);
+
+    const deletebtn = document.createElement('button');
+    deletebtn.innerHTML = '<i class = "fa fa-close"></i>';
+    deletebtn.setAttribute('id', 'deletebtn');
+    deletebtn.onclick = function () {
+        deleteTask(value);
+    };
+    document.querySelector('#div_' + tasks.indexOf(value)).appendChild(deletebtn);
+
+    if (value.completed) {
+        task.style.textDecoration = 'line-through'
+        input.checked = true;
     }
 }
